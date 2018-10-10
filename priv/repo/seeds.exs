685063ab6bac5ed4a085d0f2811a73727342ed73
Lorenzo Sinisi <lasslo.net@gmail.com>
2018-10-10 09:32:55 +0200

    Fix the seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,16 +10,37 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+Scrumpointer.Repo.delete_all(Scrumpointer.Coherence.User)
 
-Scrumpointer.Repo.delete_all Scrumpointer.Coherence.User
-
-Scrumpointer.Coherence.User.changeset(
-  %Scrumpointer.Coherence.User{}, 
-  %{
-    name: "Test User", 
-    email: "testuser@example.com", 
-    password: "secret", 
+user =
+  Scrumpointer.Coherence.User.changeset(%Scrumpointer.Coherence.User{}, %{
+    name: "Lorenzo Sinisi",
+    email: "lasslo.net@gmail.com",
+    password: "secret",
     password_confirmation: "secret",
-    confirmed_at: Ecto.DateTime.utc
-  }
-) |> Scrumpointer.Repo.insert!
+    confirmed_at: Ecto.DateTime.utc(),
+    terms_accepted_at: Ecto.DateTime.utc()
+  })
+  |> Scrumpointer.Repo.insert!()
+
+defmodule Scrumpointer.Factory.Project do
+  alias Scrumpointer.Web.Project
+
+  def build(attrs \\ %{}) do
+    %{
+      name: "A name",
+      user_id: nil,
+      team_emails: [""]
+    }
+    |> Map.merge(attrs)
+  end
+
+  def create(attrs \\ %{}) do
+    params = attrs |> build
+
+    Project.changeset(%Project{}, params)
+    |> Scrumpointer.Repo.insert()
+  end
+end
+
+project = Scrumpointer.Factory.Project.create(%{user_id: user.id})
